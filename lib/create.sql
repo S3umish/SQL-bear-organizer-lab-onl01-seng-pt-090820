782fab240f6f871621fb37551792ef582a545d3e
S1umish <sunitee.mishra@gmail.com>
2020-11-03 10:50:22 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+);
+
+
+-- id     |integer| <-- Make sure this is the table's primary key
+-- |name   |text   |
+-- |age    |integer|
+-- |gender |text   |(The choices could be "Male", "Female", "Other", or "Prefer not to say")|
+-- |color  |text   |
+-- |temperament|text|
+-- |alive  |boolean
